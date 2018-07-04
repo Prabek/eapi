4e729f01064df64b16e999e0f371dbb14dbb6e2e
prabek <prabrk79@gmail.com>
2018-07-04 22:19:46 +0545

    add rReviewController and add the paginate in Product CONTROLLER

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ReviewResource;
+use App\Model\Product;
 use App\Model\Review;
 use Illuminate\Http\Request;
 
@@ -12,10 +14,11 @@ class ReviewController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        //
-    }
+    public function index($id)
+        {   
+ return ReviewResource::collection(Product::find($id)->reviews);
+     }
+
 
     /**
      * Show the form for creating a new resource.
